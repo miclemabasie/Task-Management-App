557fb9c62c3c66f1ac50c2396aa536ad576afd86
miclemabasie <miclemabasie3@gmail.com>
2025-02-17 09:40:41 +0100

    added headline text in the home screeen

--- components/ui/Title.jsx
+++ components/ui/Title.jsx
@@ -1,12 +1,30 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
-const Title = () => {
+const Title = ({text, color, size}) => {
   return (
-    <View>
-      <Text>Title</Text>
+    <View style={styles.HeadlineContainer}>
+      <Text style={[styles.text, {color: color, fontSize: size}]}>{text}</Text>
+      <View style={styles.emptySpace}></View>
     </View>
   )
 }
 
 export default Title
+
+const styles = StyleSheet.create({
+  HeadlineContainer: {
+
+    flexDirection: "row",
+  },
+  text: {
+    flex: 2,
+    fontWeight: "bold",
+    lineHeight: 40
+  },
+
+  emptySpace: {
+    flex: 1
+  }
+
+})
